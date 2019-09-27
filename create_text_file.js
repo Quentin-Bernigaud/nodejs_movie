dc38b35e193a089358cb6424bfd11ea1fa6f06a3
Quentin <quentin@bernigaud.fr>
2019-09-27 18:30:47 +0200

    Clean code and ADD list creation

--- create_text_file.js
+++ create_text_file.js
@@ -1,22 +1,22 @@
+const {connection} = require('./database.js');
 const {writeFile, mkdir} = require("fs");
 const {join} = require('path');
 const destDir = join('./', 'movies');
 const messages = [];
 
-const pushFile = () => {
-    return new Promise( (resolve, reject) => {
-        mkdir(destDir, () => {
-            counter++;
-            const fileNameToCreate = join(destDir, 'list_of_movies.txt');
-            const content = `${movieTitle}`;
-            const message = `Chemin du fichier qui vient d'être créé : ${fileNameToCreate}`;
-            writeFile(fileNameToCreate, content, (err) => {
-            if (err) reject(err);
-            messages.push(message);
-            })
-        })
-    })
-};
-
-
-module.exports = {pushFile};
+connection.query(`SELECT * FROM movie`, (error, all_movies) => {
+    // const createFile = () => {
+        return new Promise( (resolve, reject) => {
+            mkdir(destDir, () => {
+                const fileNameToCreate = join(destDir, 'list_of_movies.txt');
+                const content = `${all_movies}`;
+                const message = `Chemin du fichier qui vient d'être créé : ${fileNameToCreate}`;
+                writeFile(fileNameToCreate, content, (err) => {
+                if (err) reject(err);
+                messages.push(message);
+                });
+            });
+        });
+    // }; 
+});
+    
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const axios = require('axios');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const {pushFile} = require('./file.js')
 const {connection} = require('./database.js');
 
 const server = express();
@@ -13,7 +11,6 @@ server.use(bodyParser.json());
 server.use(cors());
 
 router.get('/', (req, res) => {
-
 })
 
 router.get('/movies', (req, res) => {
@@ -26,8 +23,6 @@ router.get('/movies', (req, res) => {
 router.post('/add_movie', (req, res) => {
     const url = req.body;
     console.log(url);
-    console.log(url.title);
-    console.log(url.date);
     let sql = `INSERT INTO Movie (title, date) VALUES ('${url.title}', '${url.date}');`, error;
     if (error) res.status(201).json({ error });
     res.status(201).json(url);
@@ -36,9 +31,7 @@ router.post('/add_movie', (req, res) => {
 
 router.post('/edit_movie', (req, res) => {
     const url = req.body;
-    console.log(url.id);
-    console.log(url.title);
-    console.log(url.date);
+    console.log(url);
     let sql = `UPDATE Movie SET title = '${url.title}', date = '${url.date}' WHERE id = ${url.id}`, error;
     if (error) res.status(201).json({ error });
     res.status(201).json(url);
@@ -47,42 +40,12 @@ router.post('/edit_movie', (req, res) => {
 
 router.post('/delete_movie', (req, res) => {
     const url = req.body;
-    console.log(url.id);
-    console.log(url.title);
-    console.log(url.date);
+    console.log(url);
     let sql = `DELETE FROM Movie WHERE id = ${url.id}`, error;
     if (error) res.status(201).json({ error });
     res.status(201).json(url);
     connection.query(sql);
 });
-
-// router.get('/add_movie/:title/:date', (req, res) => {
-    // connection.query(`INSERT INTO Movie (title, date) VALUES ('${movie_title}', '${movie_date}');`, (err, add_movie) => {
-    //     if (error) res.status(201).json({ error });
-    //     res.status(201).json({ add_movie });
-    // })
-// });
-
-// router.get('/user/:id', (req, res) => {
-//     const { id } = req.params;
-//     connection.connect();
-//     connection.query(`SELECT * FROM user WHERE id = ${id}`, (error, results) => {
-//         if (error) res.status(201).json({ error });
-//         res.status(201).json({ user: results });
-//     })
-//     console.log(id);
-// })
-
-
-
-    // axios.get(url)
-    // .then(response => res.status(201).json({contenu : req.body}))
-    // .catch(err => res.status(201).json({err}));
-    // res.status(201).json(req.body);
-    // connection.query(`INSERT INTO Movie (title, date) VALUES ('${req.body}');`, (error, add_movie) => {
-    //     if (error) res.status(201).json({ error });
-    //     res.status(201).json({ add_movie });
-    // })
 
 server.use(router);
 server.listen(port, () => {
